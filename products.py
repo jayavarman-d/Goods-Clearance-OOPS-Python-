b00e816439a62ff1a8a1b2c69cc3eeb9ff04d2d3
jayavarman-d <jayavarman866@gmail.com>
2024-12-03 21:24:38 +0530

    getter setter

--- products.py
+++ products.py
@@ -13,11 +13,20 @@ class Products:
     def get_prod_id(self):
         return self.__product_id
 
+    def set_prod_id(self, product_id):
+        self.__product_id = product_id
+
     def get_prod_name(self):
         return self.__product_name
 
+    def set_prod_name(self, product_name):
+        self.__product_name = product_name
+
     def get_quantity(self):
         return self.__quantity
+
+    def set_quantity(self, quantity):
+        self.__quantity = quantity
 
     def get_price(self):
         return self.__price
